5ea45aad56a7d264c2949f0cb4064521e973eefb
mega <megananthank4@gmail.com>
2021-12-28 19:09:53 +0530

    added hello package

---
+++ src/hello/hello2.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class hello2 {
+	public static void main(String[] args) {
+		helloworld.main(args);
+	}
+
+}
---
+++ src/hello/helloworld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class helloworld {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+	}
+
+}
