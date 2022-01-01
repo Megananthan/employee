0eacb3e87742b6269f31af1431ee45d1a4febbca
mega <megananthank4@gmail.com>
2022-01-01 22:16:50 +0530

    Changed project to maven

---
+++ src/main/java/hello/helloworld.java
@@ -0,0 +1,78 @@
+package hello;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+
+
+public class helloworld {
+
+	
+	private final Logger logger=Logger.getLogger(helloworld.class);
+	
+	private Connection con;
+	
+	public helloworld() throws IOException, ClassNotFoundException, SQLException {
+		BasicConfigurator.configure();
+		Properties prop= new Properties();
+		
+		InputStream stream=new FileInputStream("employee.properties");
+		prop.load(stream);
+		Class.forName(prop.getProperty("jdbc.driver"));
+		 con= DriverManager.getConnection(prop.getProperty("jdbc.url"), prop.getProperty("jdbc.username"), prop.getProperty("jdbc.password"));
+		
+	}
+
+
+	public void insert() throws SQLException {
+		int id = 1;
+		Statement st = con.createStatement();
+		String a;
+		for (int i = 0; i < 3; i++) {
+			a = "insert into employee values (" + (id + i) + ",'name" + (i + 1) + "'," + (31 + i) + ");";
+			logger.info(a);
+			st.executeUpdate(a);
+		}
+		st.close();
+	}
+
+	public ResultSet select() throws SQLException {
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM employee.employee;");
+//		while (rs.next()) {
+//			logger.info(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3));
+//		}
+		stmt.close();
+		return rs;
+	}
+
+	public void delete() throws SQLException {
+
+		Statement st = con.createStatement();
+		st.executeUpdate("delete from employee;");
+		st.close();
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+		
+		
+		
+		
+		
+		
+	
+	}
+
+}
---
+++ src/test/java/hello/employeeTest.java
@@ -0,0 +1,33 @@
+package hello;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class employeeTest {
+	
+	
+
+	@Test
+	public void sayHello() throws ClassNotFoundException, IOException, SQLException {
+		System.out.println("Hello");
+		helloworld he=new helloworld();
+		he.delete();
+		he.insert();
+		ResultSet rs=he.select();
+		int i=0;
+		System.out.println("b4");
+//		
+//		while (rs.next()) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		System.out.println(i);
+//		assertEquals(3,i);
+	}
+	
+}
